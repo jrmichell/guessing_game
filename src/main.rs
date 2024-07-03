8fbb5e140b43cec2453f16faf5f566a8f062806b
Jack Michell <me@jmich.dev>
2024-07-03 09:29:12 -0500

    chore: added comments to document what portions of the code do

--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,25 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
+    // Generate a random number between 1 and 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
+    // Loop until the user guesses the number
     loop {
         println!("Please input your guess.");
 
         let mut guess = String::new();
 
+        // Read the user input and return an error if it fail to read the line
         io::stdin()
-            .read_line(&mut guess) // mutable with &mut
+            .read_line(&mut guess)
             .expect("Failed to read line");
 
+        // Convert the user input to a number and return an error if it fail to parse the number
         let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
         println!("You guessed: {guess}");
 
+        // Compare the user input with the secret number
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
